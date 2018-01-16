d4c213ad22e1416b7173628621c4f92c56ed7b5d
timdamra <hdamra@gmail.com>
2018-01-16 14:26:31 -0600

    cors

---
+++ server/fetchTweets.js
@@ -0,0 +1,10 @@
+const Twitter = require('twitter');
+
+const client = new Twitter({
+  consumer_key: 'bFi3x7EFgu2iOqevEg0xZ5KCV',
+  consumer_secret: 'GOcgdGP0Ve8Gj8BuokncxpVXXZfAWj3SBiCch9kZ1RN2D2DEXK',
+  access_token_key: '875231032961884160-y2zwSn8vFnTE3qdJG7jClvUY8jDEnOI',
+  access_token_secret: 'ddcTBdJ71QtS3ECz3E2U3VnQCdMJfI7h6W1E6EaUu8yQ8'
+});
+
+module.exports = client;
--- server/index.js
+++ server/index.js
@@ -8,26 +8,53 @@ const CoinMarketCap = require('coinmarketcap-api');
 const ccxt = require('ccxt');
 const path = require('path');
 
+const fetchTweets = require('./fetchTweets');
+
 const app = express();
 const client = new CoinMarketCap();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.get('/list', async (req, res) => {
   let exchange = new ccxt.coinmarketcap();
 
   try {
     let data = await exchange.loadMarkets();
-    let news = await axios.get(
-      'https://newsapi.org/v2/top-headlines?sources=crypto-coins-news&apiKey=a8155b1f3f7f45ab96a28fdd3f99721d'
+    let twitterData = await fetchTweets.get(
+      'https://api.twitter.com/1.1/search/tweets.json',
+      {
+        q: 'cryptocurrency',
+        lang: 'en'
+      }
     );
+
     let dataArray = Object.values(data).slice(0, 750);
     let coins = dataArray
       .filter(val => val.quote === 'USD')
       .map(val => val.info);
 
-    res.status(200).send({ coins, news: news.data });
+    let tweets = twitterData.statuses.map(val => {
+      return {
+        text: val.text,
+        name: val.user.name,
+        screenName: val.user.screen_name,
+        followers: val.user.followers_count,
+        friends: val.user.friends_count,
+        retweetCount: val.retweet_count
+      };
+    });
+
+    res.status(200).send({ coins, tweets });
   } catch (err) {
     console.log(err);
     res.status(400).send('Something went wrong!');
