14e07ef7ef054b34d195eed38994eeee60b4bd9c
timdamra <hdamra@gmail.com>
2018-01-24 08:41:24 -0600

    heroku fix3

--- server/index.js
+++ server/index.js
@@ -16,12 +16,11 @@ const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '..', 'dist');
 
 app.use(morgan('tiny'));
+app.use(express.static(publicPath));
 
 require('./dataRoutes')(app, ccxt);
 
-app.use(express.static(publicPath));
-
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
