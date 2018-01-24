46adbd5d7c37b377f3dd1d605ae0ec2269e0401e
timdamra <hdamra@gmail.com>
2018-01-24 00:36:55 -0600

    init run 8

--- index.js
+++ index.js
@@ -16,17 +16,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 
-if (process.NODE_ENV === 'development') {
-  app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-  });
-}
-
 require('./server/dataRoutes')(app, ccxt);
 
 if (process.env.NODE_ENV === 'production') {
@@ -39,9 +28,9 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.send('Something went wrong!');
+  res.send(err);
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is on port 3000`);
+  console.log(`Server is on port ${PORT}`);
 });
