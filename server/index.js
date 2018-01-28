901c4ca44a9a226777c1d3b4555f523875476d2f
timdamra <hdamra@gmail.com>
2018-01-28 00:05:38 -0600

    fixed refresh14

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 require('./dataRoutes')(app, ccxt);
-app.use(express.static(__dirname, '..', 'dist'));
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
